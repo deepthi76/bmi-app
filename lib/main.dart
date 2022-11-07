85533225050b73fa28d8fd806cc5520d29acb2de
Deepthi <ashok@Ashoks-MacBook-Pro.local>
2022-11-07 20:58:48 +0530

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double height = 100;
+  int weight = 40;
+  double result = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("BMI Calculator"),
+      ),
+      body: Column(
+        children: [
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0,),
+                  color: Colors.grey,
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "HEIGHT",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Text(
+                            "${height.round()}",
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 60.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          const SizedBox(width: 5.0,),
+                          const Text(
+                            "CM",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Slider(
+                        value: height,
+                        min: 80.0,
+                        max: 220.0,
+                        onChanged:(value){
+                          setState(() {
+                            height = value;
+                          });
+                        },
+                        activeColor: Colors.blue,
+                        inactiveColor: Colors.black45,
+                        thumbColor: Colors.black,
+                      ),
+                    ]
+                ),
+              ),
+            ),
+          ),
+
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "WEIGHT",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white.withOpacity(0.8),
+                              ),
+                            ),
+                            Row(
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              Text(
+                              "${weight}",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 60.0,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+
+                            const Text(
+                              "Kg",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            ]
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FloatingActionButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      weight--;
+                                    });
+                                  },
+                                  heroTag: 'Weight-',
+                                  mini: true,
+                                  child: const Icon(
+                                    Icons.remove,
+                                  ),
+                                ),
+                                FloatingActionButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      weight++;
+                                    });
+                                  },
+                                  heroTag: 'Weight+',
+                                  mini: true,
+                                  child: Icon(
+                                    Icons.add,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0,),
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20.0,),
+
+                  ],
+                ),
+              )
+          ),
+
+          Container (
+            width: 400,
+            height: 55.0,
+            color: Colors.pink,
+            child: MaterialButton(
+              onPressed: (){
+                setState(() {
+                  result= weight / pow(height/100, 2);
+                });
+
+              },
+              child: const Text(
+                "CALCULATE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+
+
+          Container (
+          width: 400,
+          height: 55.0,
+          color: Colors.green,
+
+             child: Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [Text("BMI: ${(result).round()}",
+                            style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25.0,
+                            ),
+                           ),
+                          ],
+                        ),
+                     ),
+        ],
+      ),
+    );
+  }
+}
